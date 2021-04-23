a88a8c9c14870cbcf001048fdce0e63492c90846
Mathieu Gravey <mathieu@mgravey.com>
2021-04-23 15:52:03 +0200

    update the webvsite

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function runComputation(){
 		surf=Math.round(lostSurfaceInmm2/1000000*100)/100+' m&#178';
 	}
 	$('#result').html('<b>Due to your usage, '+surf+' of sea ice disappeared in Artic!!!*</b><br>*Estimation based on the work of <a href="https://science.sciencemag.org/content/354/6313/747">Notz et al., 2016</a>; CO2e/kWh data from <a href="https://www.eia.gov/tools/faqs/faq.php?id=74&t=11"">EIA</a> and <a href="https://www.eea.europa.eu/data-and-maps/daviz/co2-emission-intensity-6#tab-googlechartid_googlechartid_googlechartid_googlechartid_chart_11111">EEA</a>.')
-
+	$('#pinguing').addClass('animate__fadeInUp');
 }
 
 $(window).on('load', function(){
